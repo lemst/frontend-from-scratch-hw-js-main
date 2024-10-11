77adea7b14d860b73a0f00263867b0ceafdf1197
lemst <kate.savrasova@yandex.ru>
2024-10-11 17:37:45 +0300

    completed task-1 from lesson-4

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,22 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+    let isElement
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            isElement = true
+            break
+        } else {
+            isElement = false
+        }
+    }
+
+    return isElement
+};
+
+console.log(includesElement([1, 2, 3], 2));
+console.log(includesElement([1, 2, 3], 5));
+console.log(includesElement(['Dog', 'Cat', 'Frog'], 'Cat'));
+console.log(includesElement(['Dog', 'Cat', 'Frog'], 'Parrot'));
