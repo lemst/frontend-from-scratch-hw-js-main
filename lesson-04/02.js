7d160462804b38e9791375f16d67e92054e4e053
lemst <kate.savrasova@yandex.ru>
2024-10-13 17:47:27 +0300

    completed task-2 from lesson-4

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,21 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    let result = []
+
+    result.push(array[0])
+    for (let i = 0; i < array.length; i++) {
+        let isElement = includesElement(result, array[i])
+        if (isElement === false) {
+            result.push(array[i])
+        }
+    }
+
+    return array.length === 0 ? result = [] : result
+};
+
+console.log(findUniqueElements([2, 2, 2, 2, 2, 2, 5, 3, 4, 5, 8, 7, 7, 8]));
+console.log(findUniqueElements([1, 1, 1, 1, 1, 1, 1, 1]));
+console.log(findUniqueElements([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(findUniqueElements([]));
